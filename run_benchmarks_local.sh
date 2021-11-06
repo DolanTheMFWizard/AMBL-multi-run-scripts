28cae887b4a4edf18ac6b23c597b62ff2a79ec1c
DolanTheMFWizard <89041085+DolanTheMFWizard@users.noreply.github.com>
2021-11-06 12:41:38 -0700

    updated run times

--- run_benchmarks_local.sh
+++ run_benchmarks_local.sh
@@ -10,7 +10,7 @@ echo "Executing benchmarks..."
 # AWS_ARGS=--extra_args "-m aws -p 1500"
 DEFAULT_ARGS="--git_user DolanTheMFWizard --workspace benchmark --nohup"
 
-BENCHMARK=ag
+BENCHMARK=classification_AMLB
 BRANCH="nll"
 CONSTRAINT=4h8c
 
@@ -18,37 +18,37 @@ FRAMEWORK="AutoGluon:latest"
 CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
 $MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
 
-sleep 3600
+sleep 7200
 
 FRAMEWORK="Refit_Full:latest"
 CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
 $MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
 
-sleep 3600
+sleep 7200
 
 FRAMEWORK="Vector_Scaling_best:latest"
 CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
 $MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
 
-sleep 3600
+sleep 7200
 
 FRAMEWORK="Temperature_Scaling:latest"
 CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
 $MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
 
-sleep 3600
+sleep 7200
 
 FRAMEWORK="TempScaling_bestquality:latest"
 CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
 $MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
 
-sleep 3600
+sleep 7200
 
 FRAMEWORK="Vector_Scaling:latest"
 CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
 $MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
 
-sleep 3600
+sleep 7200
 
 FRAMEWORK="AutoGluon_bestquality:latest"
 CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
