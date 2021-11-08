e884d2cfeea8926c9b8ce1276e1b04802cb61e46
DolanTheMFWizard <89041085+DolanTheMFWizard@users.noreply.github.com>
2021-11-08 14:11:31 -0800

    Update run_benchmarks_local.sh

--- run_benchmarks_local.sh
+++ run_benchmarks_local.sh
@@ -14,44 +14,21 @@ BENCHMARK=classification_AMLB
 BRANCH="nll"
 CONSTRAINT=4h8c
 
-FRAMEWORK="AutoGluon:latest"
-CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
-$MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
+frameworks_array=( 
+    'AG_VS'
+    'Vector_Scaling'
+    'AG_best_VS'
+    'Vector_Scaling_best'
+)
 
-sleep 7200
+for framework in "${frameworks_array[@]}"
+do
+    FRAMEWORK="${framework}:latest"
+    echo "${FRAMEWORK} running"
+    CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
+    $MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 5000"
 
-FRAMEWORK="Refit_Full:latest"
-CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
-$MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
-
-sleep 7200
-
-FRAMEWORK="Vector_Scaling_best:latest"
-CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
-$MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
-
-sleep 7200
-
-FRAMEWORK="Temperature_Scaling:latest"
-CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
-$MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
-
-sleep 7200
-
-FRAMEWORK="TempScaling_bestquality:latest"
-CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
-$MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
-
-sleep 7200
-
-FRAMEWORK="Vector_Scaling:latest"
-CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
-$MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
-
-sleep 7200
-
-FRAMEWORK="AutoGluon_bestquality:latest"
-CUSTOM_ARGS="--framework $FRAMEWORK --benchmark $BENCHMARK --constraint $CONSTRAINT"
-$MYDIR/run_benchmark_local.sh --branch $BRANCH $CUSTOM_ARGS $DEFAULT_ARGS --extra_args "-m aws -p 1500"
+    sleep 18000
+done
 
 echo "All benchmarks executed."
